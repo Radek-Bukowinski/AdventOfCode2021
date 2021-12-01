9d8d7a15138cb94dd25e09901981cda1a406c184
Presisor <74270526+Radek-Bukowinski@users.noreply.github.com>
2021-12-01 16:55:27 +0000

    Update solution2.py

--- Day 1/solution2.py
+++ Day 1/solution2.py
@@ -1,1 +1,8 @@
-
+with open('data.txt', 'r') as DATAFILE:
+  DATA = DATAFILE.read().splitlines()
+  increase_count = 0
+  for x in range(len(DATA)):
+    if (len(DATA) - x) > 3:
+      if((int(DATA[x + 3]) - int(DATA[x])) > 0):
+        increase_count += 1
+  print(increase_count)
